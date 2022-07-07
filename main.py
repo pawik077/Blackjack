edacee904ca6cbedb1de33e713cfec6c869b7f72
pawik077 <pawik.r.s.077@gmail.com>
2022-07-07 18:09:42 +0200

    Added first card dealing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random as rd
 CARD_SUITS = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
 CARD_SUIT_SYMBOLS = {'Spades': '♠', 'Hearts': '♥', 'Diamonds': '♦', 'Clubs': '♣'}
 CARD_RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
@@ -10,9 +11,9 @@ class Card:
 		self.name = self.get_name()
 
 	def get_value(self):
-		if self.rank == "A":
+		if self.rank == 'A':
 			return 11
-		elif self.rank == "J" or self.rank == "Q" or self.rank == "K":
+		elif self.rank == 'J' or self.rank == 'Q' or self.rank == 'K':
 			return 10
 		else:
 			return int(self.rank)
@@ -22,13 +23,38 @@ class Card:
 	def __repr__(self):
 		return self.name
 
-def main():
+def create_deck():
 	deck = []
 	for suit in CARD_SUITS:
 		for rank in CARD_RANKS:
 			deck.append(Card(suit, rank))
-	print(deck)
-	pass
+	return deck
+
+def main():
+	deck = create_deck()
+	player_cards = []
+	dealer_cards = []
+	player_score = 0
+	dealer_score = 0
+	while len(player_cards) < 2:
+		player_cards.append(deck.pop(rd.randint(0, len(deck))))
+		player_score += player_cards[-1].value
+		if len(player_cards) == 2:
+			if player_cards[0].rank == player_cards[1].rank == 'A':
+				player_cards[0].value = 1
+				player_score -= 10
+		dealer_cards.append(deck.pop(rd.randint(0, len(deck))))
+		dealer_score += dealer_cards[-1].value
+		if len(dealer_cards) == 2:
+			if dealer_cards[0].rank == dealer_cards[1].rank == 'A':
+				dealer_cards[0].value = 1
+				dealer_score -= 10
+		print("Player cards:", player_cards)
+		print("Dealer cards:", dealer_cards)
+		print("Player score:", player_score)
+		print("Dealer score:", dealer_score)
+
+	
 
 if __name__ == '__main__':
 	main()
